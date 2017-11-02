2678b079fd71821b69da5993679bbf264f1f26e2
Connor Hopkins <connorjhopkins@gmail.com>
2017-11-01 23:21:40 -0700

    Update FacebookPostbot.java

--- src/facebookpostbot/FacebookPostbot.java
+++ src/facebookpostbot/FacebookPostbot.java
@@ -1,19 +1,27 @@
-import java.io.IOException;
 import org.json.JSONException;
-
 /**
  *
  * @author Connor J Hopkins
  */
 public class FacebookPostbot {
-    private static String JSON_URL = "http://west.wwu.edu/stcworkshops/workshop_json.asp";
-    private static String STC_PAGE = "https://stc.wwu.edu/workshops";
+    private static final String JSON_URL = "http://west.wwu.edu/stcworkshops/workshop_json.asp";
+    private static final String STC_PAGE = "https://stc.wwu.edu/workshops";
+    
     public static void main(String[] args) {
-    try {
-        post p = new post(this.JSON_URL, this.STC_PAGE);
-        p.makePost();
-    } catch(IOException | JSONException e) {
-        e.printStackTrace();
+        
+    boolean go = true;
+    int count = 0;
+    while(go) {
+        try {
+            post p = new post(JSON_URL, STC_PAGE);
+            p.makePost();
+            go = false;
+        } catch(JSONException e) {
+            if(++count > 2) {
+                System.out.println("\nFAILED TO MAKE POST \n\n" + e);
+                go = false;
+            }
+        }
     }
   }
 }
