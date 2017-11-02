8f46e14a48cce3333956627b428efac7cc11353d
Connor Hopkins <connorjhopkins@gmail.com>
2017-11-01 23:20:22 -0700

    Update weeklyMessage.java

--- src/facebookpostbot/weeklyMessage.java
+++ src/facebookpostbot/weeklyMessage.java
@@ -1,4 +1,5 @@
 import java.text.ParseException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,19 +15,16 @@ import org.jsoup.nodes.Document;
  * @author Connor J Hopkins
  */
 public class weeklyMessage {
-    private String weeklyMessage, JSON_URL;
+    private final String weeklyMessage, JSON_URL;
     private final JSONArray workshopArray;
     
     public weeklyMessage(String url) throws JSONException{
 	this.JSON_URL = url;
-	try {
-            this.workshopArray = jsonUrlToObject(this.JSON_URL).getJSONArray("workshops");
-        } catch(JSONException e) {
-            e.printStackTrace();
-        }
-	this.weeklyMessage = getMessage(this.workshopArray);
+	this.workshopArray = jsonUrlToObject(this.JSON_URL).getJSONArray("workshops");
+        this.weeklyMessage = populateMessage(this.workshopArray);
     }
-    private String getMessage(JSONArray workshopArray) {
+    
+    private String populateMessage(JSONArray workshopArray) throws JSONException {
 	Date nextWeek = addWeek(new Date());
 	JSONObject workshop;
         String workshopDate, message;
@@ -34,22 +32,15 @@ public class weeklyMessage {
 		
         int len = workshopArray.length();
         for(int i = 0; i < len; i++) {
-            
             workshop = workshopArray.getJSONObject(i);
             workshopDate = workshop.getString("date");
-            
-            if(earlyEnough(stringToDate(workshopDate), nextWeek))
-                message+=("\n"+workshop.getString("name") + "\n" 
-                              + "---"+workshop.getString("date")+ "at " + workshop.getString("start")) + "\n";
+            if(stringToDate(workshopDate).compareTo(nextWeek) <= 0) //true iff date1 is before or == date2
+                message+=   "\n" + workshop.getString("name") + "\n" + 
+                            "---"+ workshop.getString("date") + "at "+ workshop.getString("start") + "\n";
         }
         message+="\nTo sign up for a workshop, please visit our website at the link below. We hope to see you soon!\n";
-		return message;
-	}
-	
-    private boolean earlyEnough(Date date1, Date date2) {
-        return (date1.compareTo(date2) <= 0); //true iff date1 is before or = date2
+        return message;
     }
-    
     /*
     * Add 7 days to a Date object
     */
@@ -73,23 +64,25 @@ public class weeklyMessage {
 
         return date;
     }
-	
-    private JSONObject jsonUrlToObject(String JSON_URL) {
+    private JSONObject jsonUrlToObject(String JSON_URL) throws JSONException {
         Document doc;
-        try {
-            doc = Jsoup.connect(JSON_URL).get();
-        } catch(IOException e) {
-            e.printStackTrace();
-            doc = new Document("error");
-        }
+        JSONObject obj = null;
         
-        String jsonText = doc.body().text();
-        JSONObject obj;
-        try{
-            obj = new JSONObject(jsonText);
-        } catch(JSONException e) {
-            e.printStackTrace();
-            obj = new JSONObject();
+        String error = "GET JSON FROM WEBPAGE";
+        boolean go = true;
+        int count = 0;
+        while(go) {
+            try {
+                doc = Jsoup.connect(JSON_URL).get();
+                error = "CREATE JSON OBJECT";
+                obj = new JSONObject(doc.body().text());
+                go = false;
+            } catch(IOException e) {
+                if(++count > 2) {
+                    System.out.println("\nFAILED TO "+error+ "\n\n" +e);
+                    go = false;
+                }
+            }
         }
         return obj;
     }
